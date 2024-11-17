f737cf34466cfb2a2cb11c2e08293b313b67985e
mdabdulkyum1 <kyummdabdul@gmail.com>
2024-11-18 01:11:49 +0600

    set tailwind config and set google fonts poppins

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 function MainLayout() {
   return (
-    <div>
+    <div className="font-poppins">
       <Outlet></Outlet>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,8 +5,26 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'frosty-blue': '#A1C6EA', 
+        'snowy-white': '#FFFFFF',  
+        'winter-sky': '#F0F4F8',   
+        'ice-blue': '#B4D6E7',    
+        'slate-gray': '#6C7A89',  
+        'frost-green': '#A3D9A5', 
+      },
+      backgroundColor: {
+        'primary-bg': '#F0F4F8', 
+      },
+      textColor: {
+        'primary-text': '#333333',
+        'heading': '#6D9DC5',      
+      },
+      fontFamily: {
+        'poppins': ['Poppins', 'sans-serif'], 
+      },
+    },
   },
-  plugins: [require('daisyui'),],
+  plugins: [require('daisyui')],
 }
-
