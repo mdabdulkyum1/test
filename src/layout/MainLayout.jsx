f51d0b29e5283854b0348b6d92c974c5a8de1056
mdabdulkyum1 <kyummdabdul@gmail.com>
2024-11-18 00:44:11 +0600

    json data created and update mainlayout and tailwind config

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,12 +1,11 @@
-import { Outlet } from "react-router-dom"
+import { Outlet } from "react-router-dom";
 
 function MainLayout() {
-
   return (
     <div>
-        <Outlet></Outlet>
+      <Outlet></Outlet>
     </div>
-  )
+  );
 }
 
-export default MainLayout
+export default MainLayout;
