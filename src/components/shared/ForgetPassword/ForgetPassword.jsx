8cb0ff8d53a40383d802aafe7c6a317c82c974a7
mdabdulkyum1 <kyummdabdul@gmail.com>
2024-11-21 10:42:49 +0600

    update ForgetPassword added notify

--- src/components/shared/ForgetPassword/ForgetPassword.jsx
+++ src/components/shared/ForgetPassword/ForgetPassword.jsx
@@ -1,65 +1,63 @@
-import { useContext } from "react"
-import { AuthContext } from "../../../providers/AuthProviders"
+import { useContext } from "react";
+import { AuthContext } from "../../../providers/AuthProviders";
 import { useNavigate } from "react-router-dom";
-
+import { toast } from "react-toastify";
 
 function ForgetPassword() {
+  const { emailRef, handelResetPasswordWithEmail } = useContext(AuthContext);
 
-    const { emailRef, handelResetPasswordWithEmail } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-    const navigate = useNavigate()
+  const handelReset = (e) => {
+    e.preventDefault();
+    const email = e.target.email.value;
 
-    const handelReset = e => {
-        e.preventDefault();
+    handelResetPasswordWithEmail(email).then(() => {
+      notify();
+      window.open("https://mail.google.com", "_blank");
+      navigate("/login");
+    });
+    e.target.reset();
+  };
 
-
-        const email = e.target.email.value;
-
-        handelResetPasswordWithEmail(email)
-        .then(() => {
-            console.log("Password reset email sent!");
-            window.open("https://mail.google.com", "_blank");
-            navigate("/login");
-          })
-          .catch((error) => {
-            const errorCode = error.code;
-            const errorMessage = error.message;
-            console.log(errorCode)
-            console.log(errorMessage)
-          });
-
-
-        e.target.reset();
-    }
-
+  const notify = () => {
+    toast.info("Password reset email sent!", {
+      position: "top-center",
+      autoClose: 2000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "light",
+    });
+  };
 
   return (
     <div className="my-6">
-       <h1 className="text-xl font-bold my-4 text-center">Forget Password</h1> 
-       <div className="w-1/3 mx-auto">
-          <form onSubmit={handelReset}>
+      <h1 className="text-xl font-bold my-4 text-center">Forget Password</h1>
+      <div className="w-1/3 mx-auto">
+        <form onSubmit={handelReset}>
           <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="email"
-                name="email"
-                defaultChecked="gmail"
-                value={emailRef.current?.value ? emailRef.current.value :""}
-                placeholder="email"
-                className="input input-bordered"
-                required
-              />
-            </div>
-            
-            <div className="form-control mt-6">
-              <button className="btn bg-ice-blue">Reset Password</button>
-            </div>
-          </form>
-       </div>
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              name="email"
+              value={emailRef.current?.value && emailRef.current.value}
+              placeholder="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn bg-ice-blue">Reset Password</button>
+          </div>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ForgetPassword
+export default ForgetPassword;
